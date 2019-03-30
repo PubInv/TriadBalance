ff2b3d14cfcaa46876414b485ec7c1ba8731e5f1
Robert L. Read <read.robert@gmail.com>
2019-03-30 10:32:44 -0500

    major improvements aimed at producing a JSFIDDLE

--- TriadBalanceExample.js
+++ TriadBalanceExample.js
@@ -15,33 +15,33 @@
 //   You should have received a copy of the GNU General Public License
 //   along with TriadBalance.  If not, see <https://www.gnu.org/licenses/>.
 
-
-var THREE_DIMENSIONS = { "MBS": ["Mind","Body","Spirit"],
-                         "HHH": ["Head","Heart","Hands"],
-                         "PSM": ["Physical","Social","Mental"],
-                         "SEF": ["Strength","Endurance","Flexibility"],
-                         "FSH": ["Father","Son","Holy Spirit"]
-                       };
-var CURRENT_D = "MBS";
-
-var WORLD_TRIANGLE_COORDS;
+var CUR_POINT;
+var CUR_TRIANGLE_COORDS;
 var W;
 var H;
+var Whalf;
+var Hhalf;
+var NORM_TO_USE;
+var LABELS;
 
-// const SIDE_LENGTH_PIXEL = 300;
-// const SIDE_LENGTH_HEIGHT = SIDE_LENGTH_PIXEL * TRIANGLE_HEIGHT;
+function set_norm_to_use(norm) {
+  NORM_TO_USE = norm;
+}
 
-let GLOBAL_SVG_ID = "create_svg";
-let FONT_SIZE_RATIO_TO_HEIGHT = 1/20;
+function set_labels(labels) {
+  LABLES = labels;
+}
 
-function get_world_triangle() {
-  var GLOBAL_SVG_ELT = document.getElementById(GLOBAL_SVG_ID);
+function get_world_triangle(svg_id) {
+  var svg_elt = document.getElementById(svg_id);
 
   // Now the question is can we make this work with any width element?
-  W = GLOBAL_SVG_ELT.clientWidth;
-  H = GLOBAL_SVG_ELT.clientHeight;
-  
-  let SIDE_LENGTH_PIXEL = W * (3/5);
+  let W = svg_elt.clientWidth;
+  let H = svg_elt.clientHeight;
+  var min = Math.min(W,H);
+
+  // This could be a parameter...
+  let SIDE_LENGTH_PIXEL = min * (3/5);
   
   const TRIANGLE_WIDTH = 1;
   const TRIANGLE_HEIGHT = Math.sqrt(3)/2;
@@ -55,17 +55,9 @@ function get_world_triangle() {
   return wtc_vector;
 }
 
-
-// These function convert my abstract coordinates
-// to svg coordinates.
-
-var CUR_POINT;
-var CUR_TRIANGLE_COORDS;
-
-
-
-function render_svg(svg,wtc,d_labels,fs) {
-
+function render_svg(svg,wtc,d_labels,fs_ratio_to_height) {
+  LABELS = d_labels;
+  let fs = H * fs_ratio_to_height;
   function vpy(y) { return (-y); }
   function vpx(x) { return (x); }
 
@@ -91,7 +83,8 @@ function render_svg(svg,wtc,d_labels,fs) {
   polygon.setAttributeNS(null,"id","triad-balance-triangle");  
   svg.appendChild(polygon);
 
-  // These are the vertices....
+  // These are the vertices....these can be moved out,
+  // as they don't change until the geometry changes.
   var array = [ [ vpx(wtc[0].x),vpy(wtc[0].y) ], 
                 [ vpx(wtc[1].x),vpy(wtc[1].y) ],
                 [ vpx(wtc[2].x),vpy(wtc[2].y) ] ];
@@ -103,22 +96,26 @@ function render_svg(svg,wtc,d_labels,fs) {
     point.y = value[1];
     polygon.points.appendItem(point);
   }
-//  polygon.style.fill='lemonchiffon';
 
   // These are ugly, they should really be computed from the text.
   // In fact, since this does not change, the whole thing could go into
   // HTML and css more profitably.
   // fs is the fontsize in pixels; hopefully we caculate this.
-  
-  append_text(svg,"vertex-0","triad-vertices",array[2][0],array[2][1],-fs/2,
-              d_labels[2]
-             );
-  append_text(svg,"vertex-1","triad-vertices",array[0][0],array[0][1],fs,
-              d_labels[0]              
-             );
-  append_text(svg,"vertex-2","triad-vertices",array[1][0],array[1][1],fs,
-              d_labels[1]
-             );
+  function render_labels(svg,vertices,d_labels,fs) {
+    append_text(svg,"vertex-0","triad-vertices",
+                vertices[2][0],vertices[2][1],-fs/2,
+                d_labels[2]
+               );
+    append_text(svg,"vertex-1","triad-vertices",
+                vertices[0][0],vertices[0][1],fs,
+                d_labels[0]              
+               );
+    append_text(svg,"vertex-2","triad-vertices",
+                vertices[1][0],vertices[1][1],fs,
+                d_labels[1]
+               );
+  }
+  render_labels(svg,array,d_labels,fs);
 
 
   // this is the center of the triangle...
@@ -127,116 +124,73 @@ function render_svg(svg,wtc,d_labels,fs) {
   origin.setAttributeNS(null, 'cy', vpy(0));
   origin.setAttributeNS(null, 'r', 2);
   origin.setAttributeNS(null,"id","triangle_origin");        
-//  origin.setAttributeNS(null, 'style', 'fill: black; stroke: black; stroke-width: 1px;' );
   svg.appendChild(origin);
   
-  function add_point(tri,c) {
-    if (tri) {
+  function add_point(tri) {
       let point = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
       point.setAttributeNS(null, 'cx', vpx(tri.x));
       point.setAttributeNS(null, 'cy', vpy(tri.y));
       point.setAttributeNS(null, 'r', 4);
       point.setAttributeNS(null,"class","triangle_point");              
       svg.appendChild(point);
-      
-    }
   }
-  add_point(CUR_POINT,"red");
+  if (CUR_POINT)
+    add_point(CUR_POINT);
 }
 
 // This is tricky because click events on an SVG
 // depend on which object inside the SVG are hit.
 // We don't really want to do that, we have
-// created a global triangle space. A solution
-// that doesn't force us to become dependent on the SVG model
-// of objects rendered is to use screen coordinates.
-
-function clicked(evt,fs,svg,wtc){
-  var br = document.getElementById("container_to_have_global_coords_on_svg").getBoundingClientRect();
-  var x = evt.originalEvent.clientX - br.left;
-  var y = evt.originalEvent.clientY - br.top;
+// created a global triangle space. A solution is to
+// use screen coordinates, but only to compute a difference.
+function clicked(evt,container_id,fs,svg,wtc,labels,click_callback) {
+  var br = document.getElementById(container_id).getBoundingClientRect();
+  var x = evt.clientX - br.left;
+  var y = evt.clientY - br.top;
   // x and y are in the coordinates of the
   // SVG system; we need to convert them
   // to the coordinates of our triangle.
-  var xc = x + -W/2;  
-  var yc = -(y + -H/2);
-  XC = xc;
-  YC = yc;
+  var xc = x + -Whalf;  
+  var yc = -(y + -Hhalf);
   var triangle_coords = new THREE.Vector2(xc,yc);
 
-  // Note, we could balance and invert here to make sure we are inside the trianble!
+  // Note, we could balance and invert here to make sure we are inside the triangle!
   CUR_TRIANGLE_COORDS = triangle_coords;
-  var bal = setBalance(triangle_coords);
-  var vec = new THREE.Vector3(bal[0],bal[1],bal[2]);
-  
-  var norm_to_use = (getRadioValue("norm") == 0 ? L1 :L2);
-  var triangle_coords_inside_triangle = invertTriadBalance2to3(vec,wtc,norm_to_use);
+  let vec = TriadBalance2to3(CUR_TRIANGLE_COORDS,wtc,NORM_TO_USE);
 
-  CUR_POINT = triangle_coords_inside_triangle;
+  CUR_POINT = invertTriadBalance2to3(vec,wtc,NORM_TO_USE);
   
-  render_svg(svg,wtc,THREE_DIMENSIONS[CURRENT_D],fs);
+  render_svg(svg,wtc,LABELS,fs);
+
+  click_callback(CUR_TRIANGLE_COORDS,CUR_POINT,vec);
 }         
 
-
-
-function main() {
-  var GLOBAL_SVG_ELT = document.getElementById(GLOBAL_SVG_ID);
+function initialize_triad_diagram(svg_id,wtc,norm_to_use,labels,click_callback,fs_ratio_to_height = (1/20)) {
+  var svg_elt = document.getElementById(svg_id);
 
   // Now the question is can we make this work with any width element?
-  W = GLOBAL_SVG_ELT.clientWidth;
-  H = GLOBAL_SVG_ELT.clientHeight;
+  W = svg_elt.clientWidth;
+  H = svg_elt.clientHeight;
+  Whalf = W/2;
+  Hhalf = H/2;
+  LABELS = labels;
+  set_norm_to_use(norm_to_use);
 
-  
-  let fs = H * FONT_SIZE_RATIO_TO_HEIGHT;
-  render_svg(GLOBAL_SVG_ELT,WORLD_TRIANGLE_COORDS,THREE_DIMENSIONS[CURRENT_D],fs);
+  // This is convenient, but makes it hard for the client to
+  // use this svg for their own purposes, which is probably okay
+  // for this use...
+  svg_elt.setAttribute("viewBox", `-${Whalf} -${Hhalf} ${W} ${H}`);   
 
-  function set_dimension_labels(cur) {
-    $("#d0l").text(THREE_DIMENSIONS[cur][0] + ":");
-    $("#d1l").text(THREE_DIMENSIONS[cur][1] + ":");
-    $("#d2l").text(THREE_DIMENSIONS[cur][2] + ":");
-  }
-  function set_and_render(cur) {
-    CURRENT_D = cur;
-    set_dimension_labels(CURRENT_D);
-    render_svg(GLOBAL_SVG_ELT,WORLD_TRIANGLE_COORDS,THREE_DIMENSIONS[CURRENT_D],fs);        
-  }
-  
-  $("#MBS").click(() => { set_and_render("MBS");});
-  $("#HHH").click(() => { set_and_render("HHH");});
-  $("#PSM").click(() => { set_and_render("PSM");});
-  $("#SEF").click(() => { set_and_render("SEF");});
-  $("#FSH").click(() => { set_and_render("FSH");});
+  render_svg(svg_elt,wtc,labels,fs_ratio_to_height);
 
-  // Catch the norm change and recompute...
-  $(':radio[name="norm"]').change(function() {
-    setBalance(CUR_POINT);
-  });
-
-  $("#container_to_have_global_coords_on_svg").click((evt) => clicked(evt,fs,GLOBAL_SVG_ELT,WORLD_TRIANGLE_COORDS));
+  svg_elt.addEventListener(
+    "click",
+    (evt) =>
+      clicked(evt,container_id,fs_ratio_to_height,svg_elt,wtc,labels,click_callback)
+  );
 }
 
 
-function getRadioValue(name) {
-  var rates = document.getElementsByName(name);
-  var rate_value;
-  for(var i = 0; i < rates.length; i++){
-    if(rates[i].checked){
-      rate_value = i;
-    }
-  }
-  return rate_value;
-}
-
-
-function setBalance(atp) {
-  var norm_to_use = (getRadioValue("norm") == 0 ? L1 :L2);
-  let bal = TriadBalance2to3(atp,WORLD_TRIANGLE_COORDS,norm_to_use);
-
-  $( "#d0" ).text( (bal.x * 100).toFixed(0) +  "%" );
-  $( "#d1" ).text( (bal.y * 100).toFixed(0) +  "%" );
-  $( "#d2" ).text( (bal.z * 100).toFixed(0) +  "%" );
-  return [bal.x,bal.y,bal.z];
-}
 
 
 
